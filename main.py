e0d96135427d2d27ea0315f1b9c12b78a5ef71f6
Mario Ledesma <marioledga@gmail.com>
2026-02-15 12:46:58 +0100

    feat: now hardness and speed is different for each part

--- main.py
+++ main.py
@@ -19,21 +19,17 @@ tonalidades = list(datos.keys())
 # valores predeterminados
 def restaurar_valores():
     for tonalidad in tonalidades:
-        datos[tonalidad]["dificultad"] = 5
-        datos[tonalidad]["velocidad"] = 80
+        datos[tonalidad]["cambios_recientes"] = True    
         datos[tonalidad]["dias_sin_tocarla"] = 0
-        datos[tonalidad]["cambiosRecientes"] = False
-        datos[tonalidad]["apartados"]["normal"] = True
-        datos[tonalidad]["apartados"]["arpegio"] = True
-        datos[tonalidad]["apartados"]["terceras"] = True
-        datos[tonalidad]["apartados"]["cuartas"] = False
-        datos[tonalidad]["apartados"]["melodica"] = True
+        datos[tonalidad]["cambios_recientes"] = False
+        for apartado in datos[tonalidad]["apartados"]:
+            datos[tonalidad]["apartados"].update({apartado: [True, {"v": 80, "d": 5}]})
 
 # una lista con las valocidades discretas
 velocidades_discretas = [50, 52, 54, 56, 58, 60, 63, 66, 69, 72, 76, 80, 84, 88, 92, 96, 100, 104, 108, 112, 116, 120, 126, 132, 138, 144]
 
-def aumentar_velocidad(tonalidad):
-    v = datos[tonalidad]["velocidad"]
+def aumentar_velocidad(apartado):
+    v = datos[tonalidad]["apartados"][apartado][1]["v"]
     if settings["velocidades"] == "discretas":
         if v not in velocidades_discretas:
             v = min(x for x in velocidades_discretas if x >= v)
@@ -41,10 +37,10 @@ def aumentar_velocidad(tonalidad):
             v = velocidades_discretas[velocidades_discretas.index(v) + 1]
     else:
         v = v + 1
-    datos[tonalidad]["velocidad"] = v
+    datos[tonalidad]["apartados"][apartado][1]["v"] = v
 
-def disminuir_velocidad(tonalidad):
-    v = datos[tonalidad]["velocidad"]
+def disminuir_velocidad(apartado):
+    v = datos[tonalidad]["apartados"][apartado][1]["v"]
     if settings["velocidades"] == "discretas":
         if v not in velocidades_discretas:
             v = max(x for x in velocidades_discretas if x <= v)
@@ -52,33 +48,42 @@ def disminuir_velocidad(tonalidad):
             v = velocidades_discretas[velocidades_discretas.index(v) - 1]
     else:
         v = v - 1
-    datos[tonalidad]["velocidad"] = v
+    datos[tonalidad]["apartados"][apartado][1]["v"] = v
 
 # Lo que hay que hacer al tocar la escala
 def tocar(tonalidad):
-    print("Toca " + datos[tonalidad]["mayor"] + " Mayor a ", datos[tonalidad]["velocidad"], "bpm.")
-    print("Toca " + datos[tonalidad]["menor"] + " menor a ", datos[tonalidad]["velocidad"], "bpm.")
-    ejecucion = input("¿Cómo te ha salido? Elige: perfecto, bien, o mal.")
-    if ejecucion == "perfecto":
-        datos[tonalidad]["dificultad"] -= 1
-    elif ejecucion == "mal":
-        datos[tonalidad]["dificultad"] += 1
-    elif ejecucion == "bien":
-        pass
-    else:
-        print("No es un resultado admitido.")
+    for apartado in datos[tonalidad]["apartados"]:
+        if datos[tonalidad]["apartados"][apartado][0] == True:
+            print(f"Toca {apartado} a {datos[tonalidad]["apartados"][apartado][1]["v"]}")
+            ejecucion = input("¿Cómo te ha salido? Elige: perfecto, bien, o mal.").strip().lower()
+            if efecucion in ["perfe", "perfecto", "p"]:
+                datos[tonalidad]["apartados"][apartado][1]["v"] -= 1
+            elif efecucion in ["mal", "m"]:
+                datos[tonalidad]["apartados"][apartado][1]["v"] += 1
+            elif ejecucion in ["bien", "b", "bn"]:
+                pass
+            else:
+                print("No es un resultado admitido")
+            if datos[tonalidad]["apartados"][apartado]["d"] <= 0:
+                aumentar_velocidad(apartado)
+                datos[tonalidad]["apartados"][apartado]["d"] = 5
+            elif datos[tonalidad]["apartados"][apartado]["d"] >= 8:
+                disminuir_velocidad(apartado)
+                datos[tonalidad]["apartados"][apartado]["d"] = 5
     datos[tonalidad]["dias_sin_tocarla"] = 0
-    if datos[tonalidad]["dificultad"] <= 0:
-        aumentar_velocidad(tonalidad)
-        datos[tonalidad]["dificultad"] = 5
-    elif datos[tonalidad]["dificultad"] >= 8:
-        disminuir_velocidad(tonalidad)
-        datos[tonalidad]["dificultad"] = 5
+        
+
+        
 
 
 ################################
 
-cantidad_hoy = int(input("¿Cuántas escalas vas a tocar?"))
+try:
+    cantidad_hoy = int(input("¿Cuántas escalas vas a tocar? "))
+except ValueError:
+    cantidad_hoy = 3
+    print("Número no válido, usaremos 3.")
+
 mitad_antigua = cantidad_hoy // 2
 mitad_aleatoria = cantidad_hoy - mitad_antigua
 
