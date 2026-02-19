aaa089de1b4bd3eebe5983449dde26eb17ebf99c
Mario Ledesma <marioledga@gmail.com>
2026-02-19 22:35:37 +0100

    more discrete values

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def restaurar_valores():
             datos[tonalidad]["apartados"].update({apartado: [True, {"v": 80, "d": 5}]})
 
 # una lista con las valocidades discretas
-velocidades_discretas = [50, 52, 54, 56, 58, 60, 63, 66, 69, 72, 76, 80, 84, 88, 92, 96, 100, 104, 108, 112, 116, 120, 126, 132, 138, 144]
+velocidades_discretas = [30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52, 54, 56, 58, 60, 63, 66, 69, 72, 76, 80, 84, 88, 92, 96, 100, 104, 108, 112, 116, 120, 126, 132, 138, 144]
 
 def cambiar_velocidad(tonalidad, apartado, aumentar):
     v = datos[tonalidad]["apartados"][apartado][1]["v"]
